0fd95673524ba1741a1284466ce226d57bbea20d
Rakesh <rakesh@sentientgeeks.com>
2025-04-14 20:32:15 +0530

    reverse string function added

--- scripts/loop.js
+++ scripts/loop.js
@@ -1,3 +1,5 @@
+/*
+
 const arr = [
     { name: "Rakesh", dept: "Development", desg: "Full stack Developer" },
     { name: "Alex", dept: "Development", desg: "Web Developer" },
@@ -13,3 +15,22 @@ for(let x in nob){
 for(let person of arr){
     console.log(person)
 }
+
+
+function reverseString(s) {
+    try{
+        const stringarr = s.split("");
+        const arrlength = stringarr.length; 
+        const reversed = [];
+        for(let i=arrlength-1;i>=0;i--){
+            reversed.push(stringarr[i]);
+        }
+        console.log(reversed);
+    }catch(err){
+        console.log("Error",err)
+    }
+}
+
+reverseString("gdusisagdiu");
+
+*/
