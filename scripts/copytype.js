b4cbc57b8d28835ce31f89eca01981fc613aa1b4
Rakesh <rakesh@sentientgeeks.com>
2025-04-18 21:29:19 +0530

    Higher order function code added

--- scripts/copytype.js
+++ scripts/copytype.js
@@ -1,3 +1,5 @@
+/*
+
 const obj1 = {
     name:"Rakesh Mondal",
     details:{
@@ -18,3 +20,19 @@ obj2.details.city = "Hyderabad";
 console.log(obj1);
 console.log(obj2);
 console.log(obj3);
+
+*/
+
+const original = {
+  name: "Rakesh", // primitive
+  address: { city: "Kolkata", lastname: "Mondal" }, // reference
+};
+
+const shallow = { ...original }; // Shallow copy
+
+shallow.name = "Mondal"; // new primitive value
+shallow.address.city = "Delhi"; // modifies the same nested object
+shallow.address.lastname = "Sharma"; // modifies the same nested object
+
+console.log(original.name); // Rakesh (copied by value ✅)
+console.log(original); // ❗ Delhi (shared reference ❌)
--- scripts/hof.js
+++ scripts/hof.js
@@ -1,12 +1,58 @@
-const array1 = [1, 4, 9, 16];
+/*
 
-// Pass a function to map
+const radiuses = [5, 3, 8, 4];
 
-function doubleValues(val){
-    return val*2;
+function calculateAreaOfCircle(value) {
+  const cval = Math.PI * (value * value);
+  return cval;
 }
 
-const map1 = array1.map(doubleValues);
+function calculateCircumferenceOfCircle(value) {
+  const cval = 2 * Math.PI * value;
+  return cval;
+}
 
-console.log(map1);
-// Expected output: Array [2, 8, 18, 32]
+function calculateDiameterOfCircle(value) {
+  const cval = 2 * value;
+  return cval;
+}
+
+// A higher-order function is a function that takes another function as an argument
+const calculate = (values, logic) => {
+  const results = [];
+  for (let i = 0; i < values.length; i++) {
+    results[i] = logic(values[i]);
+  }
+  return results;
+};
+// A higher-order function is a function that takes another function as an argument
+
+
+const areasOfCircle = calculate(radiuses, calculateAreaOfCircle);
+
+const circumferencesOfCircle = calculate(
+  radiuses,
+  calculateCircumferenceOfCircle
+);
+
+const diametersOfCircle = calculate(radiuses, calculateDiameterOfCircle);
+
+console.log("areasOfCircle 🔴", areasOfCircle);
+console.log("circumferencesOfCircle 🟢", circumferencesOfCircle);
+console.log("diametersOfCircle 🔵", diametersOfCircle);
+
+*/
+
+//A higher-order function is a function that returns a function as its result
+
+function a(num1){
+    return function(num2){
+        return num1+num2;
+    }
+}
+
+const res = a(5)(7);
+
+console.log(res);
+
+//A higher-order function is a function that returns a function as its result
